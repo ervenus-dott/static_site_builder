d2d1ff5944363437740804090e8a4b48c4e90e32
ervenus-dott <120420594+ervenus-dott@users.noreply.github.com>
2025-04-10 12:27:21 -0600

    add code_projects.js as a template for the navbar in basic.js

--- templates/basic.js
+++ templates/basic.js
@@ -18,6 +18,7 @@ export default function (config) {
             <li><a href="${basePath}/">Home</a></li>
 <!--            <li><a href="${basePath}/gallery/">Gallery</a></li>-->
 <!--            <li><a href="${basePath}/process/">Process</a></li>-->
+            <li><a href="${basePath}/code_projects/">Code Projects</a></li>
             <li><a href="${basePath}/blog/">Blog</a></li>
           </ul>
         </nav>
---
+++ templates/code_projects.js
@@ -0,0 +1,27 @@
+import basic from './basic.js';
+
+export default function (config) {
+    return basic({
+        ...config,
+        content: /* html */`
+<article>
+  <div>
+        <iframe 
+            src="https://ervenus-dott.github.io/web_toys/06_hello_particles.html" 
+            width="600" 
+            height="600" 
+            frameborder="0" 
+            scrolling="no">
+        </iframe>
+        <iframe 
+            src="https://ervenus-dott.github.io/web_toys/08_room_generator.html" 
+            width="600" 
+            height="600" 
+            frameborder="0" 
+            scrolling="no">
+        </iframe>
+  </div>
+</article>
+`
+    });
+};
